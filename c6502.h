350dcaae3003e126c3302ac092d315a0e66ef622
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-06-10 20:06:02 +0100

    reset

--- c6502.h
+++ c6502.h
@@ -1,12 +1,15 @@
 #ifndef C6502_H
 #define C6502_H
 
+#include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef uint8_t (*read_func_t)(uint16_t);
 typedef void (*write_func_t)(uint16_t, uint8_t);
 
-struct c6502_t {
+typedef struct {
   uint8_t stack_ptr;
   uint16_t program_counter;
   uint8_t accumulator, x, y;
@@ -32,7 +35,7 @@ struct c6502_t {
 
   read_func_t read;
   write_func_t write;
-};
+} c6502_t;
 
 c6502_t *c6502_create(read_func_t read, write_func_t write);
 void c6502_destroy(c6502_t *cpu);
@@ -50,7 +53,34 @@ bool c6502_get_break(c6502_t *cpu);
 bool c6502_get_overflow(c6502_t *cpu);
 bool c6502_get_negative(c6502_t *cpu);
 
-#ifndef C6502_IMPLEMENTATION
+// todo remove
+#define C6502_IMPLEMENTATION
+
+#ifdef C6502_IMPLEMENTATION
+
+#define RESET_VECTOR_LOW 0xFFFA
+#define STACK_PAGE 0x0100
+
+void c6502_reset(c6502_t *cpu) {
+  cpu->processor_status = 0;
+  cpu->stack_ptr = 0xFD;
+  cpu->x = cpu->y = cpu->accumulator = 0;
+  cpu->cycles = cpu->implied = cpu->oprand_address = 0;
+
+  uint8_t resetVectorLow = cpu->read(RESET_VECTOR_LOW);
+  uint8_t resetVectorHigh = cpu->read(RESET_VECTOR_LOW + 1);
+  cpu->processor_status= (resetVectorHigh << 8) | resetVectorLow;
+
+  cpu->cycles = 8;
+}
+
+c6502_t *Mos6502_create(read_func_t read, write_func_t write) {
+  c6502_t *cpu = malloc(sizeof(c6502_t));
+  cpu->read = read;
+  cpu->write = write;
+  c6502_reset(cpu);
+  return cpu;
+}
 
 #endif // C6502_IMPLEMENTATION
 
